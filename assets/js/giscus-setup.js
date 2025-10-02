f7d432baf53372b8a8d389758f948d538b7ed4f1
yuqing-zhou <62488907+yuqing-zhou@users.noreply.github.com>
2025-10-02 15:15:32 +0000

    Deploying to gh-pages from @ yuqing-zhou/yuqing-zhou.github.io@a938ed08b0fc4124ab209bd77c9348e17b7889a5 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "yuqing-zhou/yuqing-zhou1.github.io",
+    "data-repo": "yuqing-zhou/yuqing-zhou.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
