75bcc56ff3e289fe31375c4f45f019a968fb3b92
yuqing-zhou <62488907+yuqing-zhou@users.noreply.github.com>
2026-08-29 05:06:43 +0000

    Deploying to gh-pages from @ yuqing-zhou/yuqing-zhou.github.io@7b634d17848885e7cbe7d5ec081d65403701561b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,12 +37,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "https://yuqing-zhou.github.io/learning-blog/index.html";
           },
-        },{id: "books-the-godfather",
-          title: 'The Godfather',
-          description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/books/the_godfather/";
-            },},{id: "news-i-will-become-a-phd-student-george-mason-university-sparkles-smile",
+        },{id: "news-i-will-become-a-phd-student-george-mason-university-sparkles-smile",
           title: 'I will become a PhD student @ George Mason University! :sparkles: :smile:',
           description: "",
           section: "News",},{id: "news-my-first-paper-was-accepted-to-cikm-2023-it-is-about-robust-post-click-conversion-rate-prediction-grateful-to-my-co-authors-and-excited-to-meet-the-community",
@@ -63,17 +58,7 @@ ninja.data = [{
           section: "News",},{id: "news-our-paper-confidence-orchestrated-self-evolution-against-uncertain-llm-feedback-was-accepted-to-emnlp-2026",
           title: 'Our paper “Confidence-Orchestrated Self-Evolution against Uncertain LLM Feedback” was accepted to EMNLP 2026....',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{
+          section: "News",},{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
